1abf08545c8c39cfbbbedba240142e6de049becb
Alec <dowtyyalec@gmail.com>
2022-11-16 12:41:12 -0800

    Added Settings Information with a Test drop down menu for themes

---
+++ app/src/debug/java/edu/uw/tcss450lucasd12/team_4_tcss450/SettingsFragment.java
@@ -0,0 +1,97 @@
+package edu.uw.tcss450lucasd12.team_4_tcss450;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link SettingsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class SettingsFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    //Spinner spinner = spinner.findViewById(R.id.spiiner);
+    String[] themes = {"Fall", "Winter", "Spring", "Summer", "UW"};
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    private Object AdapterView;
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment SettingsFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static SettingsFragment newInstance(String param1, String param2) {
+        SettingsFragment fragment = new SettingsFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+//        spinner = spinner.findViewById(R.id.spiiner);
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SettingsFragment.this, android.R.layout.simple_spinner_item,themes);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinner.setAdapter(adapter);
+//
+//        spinner.setOnItemSelectedListener(new AdapterView<?> parent, View view, int position, long id) {
+//
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent) {
+//                String value = parent.getItemAtPosition(position).toString();
+//                Toast.makeText(SettingsFragment.this, value, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        };
+
+        //ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(SettingsFragment.this, R.array.themes, android.R.layout.simple_spinner_item);
+
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        //spinner.setAdapter(adapter);
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+}
