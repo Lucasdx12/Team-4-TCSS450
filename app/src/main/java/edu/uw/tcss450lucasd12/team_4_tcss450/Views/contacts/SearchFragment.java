c720c76e452e7f15b73e766596cb50026dca382d
alexzrosario <alexzrosario@gmail.com>
2022-12-11 23:56:03 -0800

    done

--- app/src/main/java/edu/uw/tcss450lucasd12/team_4_tcss450/Views/contacts/SearchFragment.java
+++ app/src/main/java/edu/uw/tcss450lucasd12/team_4_tcss450/Views/contacts/SearchFragment.java
@@ -1,12 +1,8 @@
-package edu.uw.tcss450lucasd12.team_4_tcss450.Views.auth.changepassword;
+package edu.uw.tcss450lucasd12.team_4_tcss450.Views.contacts;
 
-import static edu.uw.tcss450lucasd12.team_4_tcss450.utils.PasswordValidator.checkClientPredicate;
 import static edu.uw.tcss450lucasd12.team_4_tcss450.utils.PasswordValidator.checkExcludeWhiteSpace;
-import static edu.uw.tcss450lucasd12.team_4_tcss450.utils.PasswordValidator.checkPwdDigit;
 import static edu.uw.tcss450lucasd12.team_4_tcss450.utils.PasswordValidator.checkPwdLength;
-import static edu.uw.tcss450lucasd12.team_4_tcss450.utils.PasswordValidator.checkPwdLowerCase;
 import static edu.uw.tcss450lucasd12.team_4_tcss450.utils.PasswordValidator.checkPwdSpecialChar;
-import static edu.uw.tcss450lucasd12.team_4_tcss450.utils.PasswordValidator.checkPwdUpperCase;
 
 import android.os.Bundle;
 
@@ -14,7 +10,6 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,35 +19,24 @@ import android.view.ViewGroup;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import edu.uw.tcss450lucasd12.team_4_tcss450.R;
-import edu.uw.tcss450lucasd12.team_4_tcss450.Views.auth.forgotpassword.ForgotPasswordFragmentDirections;
-import edu.uw.tcss450lucasd12.team_4_tcss450.Views.auth.forgotpassword.ForgotPasswordViewModel;
-import edu.uw.tcss450lucasd12.team_4_tcss450.databinding.FragmentChangePasswordBinding;
-import edu.uw.tcss450lucasd12.team_4_tcss450.databinding.FragmentForgotPasswordBinding;
+import edu.uw.tcss450lucasd12.team_4_tcss450.databinding.FragmentSearchBinding;
+import edu.uw.tcss450lucasd12.team_4_tcss450.model.UserInfoViewModel;
 import edu.uw.tcss450lucasd12.team_4_tcss450.utils.PasswordValidator;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ChangePasswordFragment extends Fragment {
+public class SearchFragment extends Fragment {
 
-    private FragmentChangePasswordBinding binding;
-
-    private ChangePasswordViewModel mChangePasswordViewModel;
+    private FragmentSearchBinding binding;
+    private UserInfoViewModel mUserModel;
+    private SearchViewModel mSearchViewModel;
 
     private PasswordValidator mEmailValidator = checkPwdLength(2)
             .and(checkExcludeWhiteSpace())
             .and(checkPwdSpecialChar("@"));
 
-    private PasswordValidator mPassWordValidator =
-            checkClientPredicate(pwd -> pwd.equals(binding.editPassword.getText().toString()))
-                    .and(checkPwdLength(7))
-                    .and(checkPwdSpecialChar())
-                    .and(checkExcludeWhiteSpace())
-                    .and(checkPwdDigit())
-                    .and(checkPwdLowerCase().or(checkPwdUpperCase()));
-
-    public ChangePasswordFragment() {
+    public SearchFragment() {
         // Required empty public constructor
     }
 
@@ -60,14 +44,15 @@ public class ChangePasswordFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mChangePasswordViewModel = new ViewModelProvider(getActivity())
-                .get(ChangePasswordViewModel.class);
+        ViewModelProvider provider = new ViewModelProvider(getActivity());
+        mUserModel = provider.get(UserInfoViewModel.class);
+        mSearchViewModel = provider.get(SearchViewModel.class);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentChangePasswordBinding.inflate(inflater);
+        binding = FragmentSearchBinding.inflate(inflater);
         return binding.getRoot();
     }
 
@@ -75,46 +60,35 @@ public class ChangePasswordFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonChangePassword.setOnClickListener(this::attemptForgotPassword);
-        mChangePasswordViewModel.addResponseObserver(getViewLifecycleOwner(),
+        binding.buttonSearch.setOnClickListener(this::attemptSearch);
+        mSearchViewModel.addResponseObserver(getViewLifecycleOwner(),
                 this::observeResponse);
     }
-    private void attemptForgotPassword(final View button) {
+
+    private void attemptSearch(final View button) {
         validateEmail();
     }
 
     private void validateEmail() {
         mEmailValidator.processResult(
                 mEmailValidator.apply(binding.editEmail.getText().toString().trim()),
-                this::validatePassword,
+                this::verifyAuthWithServer,
                 result -> binding.editEmail.setError("Please enter a valid Email address.\n"+
                         " * Must include an @\n"+
                         " * No spaces are allowed."));
     }
 
-    private void validatePassword() {
-        mPassWordValidator.processResult(
-                mPassWordValidator.apply(binding.editPassword.getText().toString()),
-                this::verifyAuthWithServer,
-                result -> binding.editPassword.setError("Please enter a valid Password.\n"+
-                        " * Must be at least 7 characters long.\n"+
-                        " * Must include a special character (@#$%&*!?)\n"+
-                        " * Must include either a lower case or upper case letter\n"+
-                        " * Must include a digit (0-9)\n"+
-                        " * No spaces are allowed."));
-    }
     private void verifyAuthWithServer() {
-        mChangePasswordViewModel.connect(
-                binding.editEmail.getText().toString(),
-                binding.editPassword.getText().toString());
+        mSearchViewModel.connect(
+                mUserModel.getEmail(),
+                binding.editEmail.getText().toString());
         //This is an Asynchronous call. No statements after should rely on the
         //result of connect().
 
     }
+
     private void navigateToLogin() {
         binding.editEmail.setText("");
-        binding.editPassword.setText("");
-
     }
 
     /**
@@ -140,4 +114,6 @@ public class ChangePasswordFragment extends Fragment {
             Log.d("JSON Response", "No Response");
         }
     }
+
+
 }
---
+++ app/src/main/java/edu/uw/tcss450lucasd12/team_4_tcss450/Views/contacts/SearchViewModel.java
@@ -0,0 +1,87 @@
+package edu.uw.tcss450lucasd12.team_4_tcss450.Views.contacts;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+import edu.uw.tcss450lucasd12.team_4_tcss450.R;
+
+public class SearchViewModel extends AndroidViewModel {
+    private MutableLiveData<JSONObject> mResponse;
+
+    public SearchViewModel(@NonNull Application application) {
+        super(application);
+        mResponse = new MutableLiveData<>();
+        mResponse.setValue(new JSONObject());
+    }
+
+    public void addResponseObserver(@NonNull LifecycleOwner owner,
+                                    @NonNull Observer<? super JSONObject> observer) {
+        mResponse.observe(owner, observer);
+    }
+
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            try {
+                mResponse.setValue(new JSONObject("{" +
+                        "error:\"" + error.getMessage() +
+                        "\"}"));
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset())
+                    .replace('\"', '\'');
+            try {
+                JSONObject response = new JSONObject();
+                response.put("code", error.networkResponse.statusCode);
+                response.put("data", new JSONObject(data));
+                mResponse.setValue(response);
+            } catch (JSONException e) {
+                Log.e("JSON PARSE", "JSON Parse Error in handleError");
+            }
+        }
+    }
+
+    public void connect(final String myEmail, final String friendEmail) {
+        String url = getApplication().getResources().getString(R.string.base_url) + "addContact";
+        JSONObject body = new JSONObject();
+        try {
+            body.put("myEmail", myEmail);
+            body.put("friendEmail", friendEmail);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Request request = new JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                body,
+                mResponse::setValue,
+                this::handleError);
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        Volley.newRequestQueue(getApplication().getApplicationContext())
+                .add(request);
+    }
+}
